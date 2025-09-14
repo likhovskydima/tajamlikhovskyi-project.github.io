8bb73a81ff4f69fb43dff684e9413b7633903e37
likhovskydima <likhovsky.dima@nemk.ukr.education>
2025-09-14 20:51:59 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
+// 
+$(function () {
+  $(document).scroll(function () {
+	  var $nav = $(".fixed-top");
+	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height() - 10);
+	});
+});
+// 
+$('.carousel').carousel({
+  interval: 2500
+});
+// 
+var linkNav = header.querySelectorAll('div.navbar-nav > [href^="#"]'), //
+    V = 0.5;  // 
+for (var i = 0; i < linkNav.length; i++) {
+    linkNav[i].addEventListener('click', function(e) { //
+        e.preventDefault(); //
+        var w = window.pageYOffset,  // 
+            hash = this.href.replace(/[^#]*(.*)/, '$1');  // 
+        t = document.querySelector(hash).getBoundingClientRect().top,  // 
+            start = null;
+        requestAnimationFrame(step);  // 
+        function step(time) {
+            if (start === null) start = time;
+            var progress = time - start,
+                r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t));
+            window.scrollTo(0,r);
+            if (r != w + t) {
+                requestAnimationFrame(step)
+            } else {
+                location.hash = hash  // 
+            }
+        }
+    }, false);
+}
 
